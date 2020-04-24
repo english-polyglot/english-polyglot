dc3de228c9b1b730d1ac426af6d60bf2d5c0e61e
PaPa <visidoor@yandex.ru>
2020-04-24 15:41:06 +0500

    Block adBlocker
    	modified:   Templates/dive-in-english.dwt
    	modified:   Templates/index2.dwt
    	modified:   english-polyglot.com/Uses_of_English_verb_forms.html
    	modified:   english-polyglot.com/answer.html
    	modified:   english-polyglot.com/conditional-sentence.html
    	modified:   english-polyglot.com/css/css.css
    	modified:   english-polyglot.com/css/dive.css
    	modified:   english-polyglot.com/css/liquid.css
    	new file:   english-polyglot.com/css/red-placeholder.css
    	modified:   english-polyglot.com/css/style.css
    	modified:   english-polyglot.com/donate.html
    	modified:   english-polyglot.com/filmy.html
    	modified:   english-polyglot.com/fragment-avatar.html
    	modified:   english-polyglot.com/fragment-beowulf.html
    	modified:   english-polyglot.com/fragment-carnage.html
    	modified:   english-polyglot.com/fragment-matrix.html
    	modified:   english-polyglot.com/fragment-no-contry-for-old-men.html
    	modified:   english-polyglot.com/fragment-pulp-fiction.html
    	modified:   english-polyglot.com/generator-fraz.html
    	modified:   english-polyglot.com/generatory-fraz.html
    	modified:   english-polyglot.com/index.html
    	modified:   english-polyglot.com/instruction-for-generator-fraz.html
    	modified:   english-polyglot.com/js/ads.js
    	new file:   english-polyglot.com/js/engine.js
    	modified:   english-polyglot.com/js/js-15-02-2019.js
    	new file:   english-polyglot.com/js/modal.js
    	modified:   english-polyglot.com/poligloto.html
    	modified:   english-polyglot.com/stop-block.html
    	modified:   english-polyglot.com/tablicy.html
    	modified:   english-polyglot.com/urok-10.html
    	modified:   english-polyglot.com/urok-11-2-initial.html
    	modified:   english-polyglot.com/urok-11-2.html
    	modified:   english-polyglot.com/urok-11-initial.html
    	modified:   english-polyglot.com/urok-11.html
    	modified:   english-polyglot.com/urok-2-dopol.html
    	modified:   english-polyglot.com/urok-2.html
    	modified:   english-polyglot.com/urok-3.html
    	modified:   english-polyglot.com/urok-4.html
    	modified:   english-polyglot.com/urok-5.html
    	modified:   english-polyglot.com/urok-6.html
    	modified:   english-polyglot.com/urok-7-with-conditions.html
    	modified:   english-polyglot.com/urok-7.html
    	modified:   english-polyglot.com/urok-8.html
    	modified:   english-polyglot.com/urok-9.html

--- english-polyglot.com/js/ads.js
+++ english-polyglot.com/js/ads.js
@@ -1,3 +1,10 @@
+/*
+ * BlockAdBlock 3.2.1
+ * Copyright (c) 2015 Valentin Allaire <valentin.allaire@sitexw.fr>
+ * Released under the MIT license
+ * https://github.com/sitexw/BlockAdBlock
+ */
+
 (function(window) {
 	var BlockAdBlock = function(options) {
 		this._options = {
